5d43e7f0b48cb5c8afca23cc25d97bdff97bf78e
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-05 15:21:30 -0600

    chore: Update ContactPage and Footer components

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,6 +2,9 @@ const ContactPage = () => {
   return (
     <div className="text-center">
       <h1 className="font-extrabold text-4xl lg:text-6xl mt-20 text-[#6a0923]">Contact Page</h1>
+<div className="max-h-full text-center justify-center my-52">
+      <form className="text-black">Contact</form>
+      </div>
     </div>
   )
 }
