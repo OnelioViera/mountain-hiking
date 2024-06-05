29b9477db9783c8f698f737ea925f664b23600ca
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-05 08:35:09 -0600

    chore: Update Hero component in main page header

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,17 +3,17 @@ import TrailBoxes from './TrailBoxes';
 const Hero = () => {
   return (
     // <!-- Hero -->
-    <section className="bg-cover bg-center h-screen"
+    <section className="bg-cover bg-center h-auto"
       style={{ backgroundImage: "url('https://cdn.backpacker.com/wp-content/uploads/2022/04/GettyImages-600968276-scaled-e1649714272520.jpg')" }}
     >
       <div
         className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
 
-        <div className="text-center object-center mt-40">
+        <div className="text-center object-center mt-10 lg:mt-40">
           <h1 className="font-extrabold text-4xl lg:text-6xl text-[#6a0923]">Colorado Springs</h1>
           <p className="text-2xl font-mono text-white my-4 lg:text-4xl">Mountain Hiking</p>
         </div>
-        <div className='mt-20'>
+        <div className='my-8 lg:my-20'>
           <TrailBoxes />
         </div>
       </div>
