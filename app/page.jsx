488682fd089af0732a12ce876583b5c31d6c345f
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-05 08:09:53 -0600

    chore: Update main page to use Hero component for header

--- app/page.jsx
+++ app/page.jsx
@@ -1,35 +1,10 @@
-import Image from 'next/image';
-import logo from '@/public/images/hiking-logo-3.png';
 import TrailBoxes from '@/components/TrailBoxes';
+import Hero from '@/components/Hero';
 
 const MainPage = () => {
   return (
     <div className="text-center object-center">
-      <h1 className="font-extrabold text-4xl lg:text-6xl mt-20 text-[#6a0923]">Colorado Springs</h1>
-      <p className="text-2xl mt-4 lg:text-4xl">Mountain Hiking</p>
-      <div className='grid place-content-center my-8'>
-        <Image src={logo}
-          alt='Hiking Logo'
-          width={200}
-          height={200}
-        />
-      </div>
-
-      <TrailBoxes />
-
-      <div className='my-6'>
-        <Image
-          src="/images/bgimage3.png"
-          alt="External Link"
-          width={0}
-          height={0}
-          sizes='100vw'
-          className="w-full h-auto"
-        />
-
-
-      </div>
-
+      <Hero />
     </div>
   )
 }
---
+++ components/Hero.jsx
@@ -0,0 +1,24 @@
+import TrailBoxes from './TrailBoxes';
+
+const Hero = () => {
+  return (
+    // <!-- Hero -->
+    <section className="bg-cover bg-center h-screen"
+      style={{ backgroundImage: "url('https://cdn.backpacker.com/wp-content/uploads/2022/04/GettyImages-600968276-scaled-e1649714272520.jpg')" }}
+    >
+      <div
+        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+
+        <div className="text-center object-center mt-60">
+          <h1 className="font-extrabold text-4xl lg:text-6xl text-[#6a0923]">Colorado Springs</h1>
+          <p className="text-2xl mt-4 lg:text-4xl">Mountain Hiking</p>
+        </div>
+
+        <TrailBoxes />
+
+      </div>
+    </section>
+  )
+};
+
+export default Hero
