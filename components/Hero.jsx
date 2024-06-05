818af780168aad14c0e3f2de2a35efa1059fbb88
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-05 08:14:07 -0600

    chore: Update Hero component in main page header

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,13 +9,13 @@ const Hero = () => {
       <div
         className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
 
-        <div className="text-center object-center mt-60">
+        <div className="text-center object-center mt-40">
           <h1 className="font-extrabold text-4xl lg:text-6xl text-[#6a0923]">Colorado Springs</h1>
-          <p className="text-2xl mt-4 lg:text-4xl">Mountain Hiking</p>
+          <p className="text-2xl font-mono text-white my-4 lg:text-4xl">Mountain Hiking</p>
         </div>
-
-        <TrailBoxes />
-
+        <div className='mt-20'>
+          <TrailBoxes />
+        </div>
       </div>
     </section>
   )
