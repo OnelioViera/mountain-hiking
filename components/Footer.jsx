98bf9bdecda44d153174a8d49e8ef92d33800e50
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-05 19:15:06 -0600

    chore: Update Footer component to include copyright year dynamically

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,10 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="mt-auto bottom-0 z-50 py-8 bg-green-700 border-t-2 h-auto border-green-800">
       <div className="text-white text-center justify-center">
-        Colorado Springs Mountain Hiking
+        <p>&copy; {currentYear} Colorado Springs Mountain Hiking</p>
       </div>
     </div>
   )
