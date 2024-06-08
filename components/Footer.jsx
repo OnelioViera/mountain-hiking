9d84ed23dc3eb65b99c41669408cf758042db849
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-07 19:07:11 -0600

    chore: Update Footer component to remove redundant CSS class

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="mt-auto bottom-0 z-50 py-8 bg-green-700 border-t-2 h-auto border-green-800">
+    <div className="py-8 bg-green-700 border-t-2 h-auto border-green-800">
       <div className="text-white text-center justify-center">
         <p>&copy; {currentYear} Colorado Springs Mountain Hiking</p>
       </div>
