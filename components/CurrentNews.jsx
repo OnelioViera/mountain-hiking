43150202552dc76fbee97b3a4db27d6a4cf3043b
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-14 16:48:31 -0600

    Update CurrentNews component styles and content

--- components/CurrentNews.jsx
+++ components/CurrentNews.jsx
@@ -6,7 +6,7 @@ const CurrentNews = () => {
       </div>
       <div>
         <div className="container-xl lg:container m-auto lg:mb-6 mb-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 rounded-lg">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 rounded-lg px-6 md:px-6">
             <div className="bg-gray-100 p-4 rounded-lg shadow-lg">
               <h2 className="text-xl font-semibold text-[#6a0923] lg:text-2xl md:text-xl">New Trail Opening</h2>
               <p className="text-md text-gray-600 my-4">A new trail is opening in the area. The trail is 5 miles long and is perfect for hiking and biking. The trail will be open to the public on May 1st. Be sure to check it out!</p>
