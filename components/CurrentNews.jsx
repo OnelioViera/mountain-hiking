aec6bea18136da03a8fcb3912a248e7872922fd6
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-14 16:13:21 -0600

    chore: Update CurrentNews component to use custom color for heading

--- components/CurrentNews.jsx
+++ components/CurrentNews.jsx
@@ -1,7 +1,7 @@
 const CurrentNews = () => {
   return (
     <div className="">
-      <h1 className="text-2xl font-semibold text-center text-blue-600 lg:text-4xl md:text-3xl border-b-2 mb-6 py-2">Current News</h1>
+      <h1 className="text-2xl font-semibold text-center text-[#6a0923] lg:text-4xl md:text-3xl border-b-2 mb-6 py-2">Current News</h1>
     </div>
   )
 };
