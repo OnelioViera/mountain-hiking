684b4225bd005cda54663254d24cf913e68d5a90
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-26 13:13:27 -0600

    chore: Update Navbar component styles and structure

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,51 +14,55 @@ const Navbar = () => {
   return (
     <nav className='sticky top-0 z-50 bg-green-700 border-b-2 border-green-800 text-white py-3 px-4 shadow-lg'>
       <div className='mx-auto max-w-8xl px-2 sm:px-6 lg:px-8'>
-        
+
         <div className='realitive flex items-center justify-between'>
-        <Link className='flex-shrink-0 hidden md:block'  href='/'>
-              <Image src={logo} alt='Hiking Logo' width={150} height={150}
-                
+          <Link className='flex-shrink-0 hidden md:block' href='/'>
+            <Image src={logo} alt='Hiking Logo' width={150} height={150}
+
             />
-            </Link>
+          </Link>
           <div className='flex space-x-2'>
 
-            
+
             <Link
               href='/'
-              className={`${pathname === '/' ?  'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
-            HOME
+              className={`${pathname === '/' ? 'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
+              HOME
             </Link>
 
             <Link
               href='/trails'
-              className={`${pathname === '/trails' ?  'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
+              className={`${pathname === '/trails' ? 'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
               TRAILS
             </Link>
 
             <Link
               href='/groups'
-              className={`${pathname === '/groups' ?  'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
+              className={`${pathname === '/groups' ? 'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
               GROUPS
             </Link>
 
             <Link
               href='/contact'
-              className={`${pathname === '/contact' ?  'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
+              className={`${pathname === '/contact' ? 'bg-[#6a0923]' : ''} hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-white`}>
               CONTACT
             </Link>
           </div>
           <div className="hidden md:block md:ml-6">
-              <div className="flex items-center">
-                <button
-                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+            <div className="flex items-center">
+              <button
+                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
               >
-                <SignInButton />
-                <SignOutButton />
-                  <span></span>
-                </button>
-              </div>
+                <div className='flex '>
+                  <SignInButton />
+                
+                <div className='pl-4'>
+                  <SignOutButton />
+                  </div>
+                  </div>
+              </button>
             </div>
+          </div>
         </div>
 
       </div>
